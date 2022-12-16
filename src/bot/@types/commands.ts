28e53387f8ba83d9012be855c1094aed2907e9d8
Davi <davipatricio@pm.me>
2022-12-15 23:46:58 -0300

    feat: add command class & types

---
+++ src/bot/@types/commands.ts
@@ -0,0 +1,49 @@
+import {ChatInputCommandInteraction} from 'discord.js';
+import {CustomClient} from './CustomClient';
+
+/**
+ * Interface for a command run options.
+ */
+export interface CommandRunOptions {
+  /**
+   * The client which is running the command.
+   */
+  client: CustomClient;
+  /**
+   * The interaction object.
+   */
+  interaction: ChatInputCommandInteraction;
+}
+
+type CommandAutoReplyOptionsNoAutoDefer = {
+  autoDefer: false;
+  autoEphemeral: false;
+};
+
+type CommandAutoReplyOptionsAutoDefer = {
+  autoDefer: true;
+  autoEphemeral: boolean;
+};
+
+export type CommandAutoReplyOptions =
+  | CommandAutoReplyOptionsNoAutoDefer
+  | CommandAutoReplyOptionsAutoDefer;
+
+export interface CommandOptions {
+  name: string;
+  description: string;
+  category: string;
+  ownerOnly: boolean;
+  replyOptions?: CommandAutoReplyOptions;
+}
+
+/**
+ * Interface for a command.
+ */
+export class Command {
+  data!: CommandOptions;
+  constructor(_options: CommandOptions) {}
+  run(_options: CommandRunOptions): Promise<unknown> | unknown {
+    return {};
+  }
+}
