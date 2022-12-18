db9a81f5467625bd165a12c014b9f770f26302f5
Davi <davipatricio@pm.me>
2022-12-18 00:45:38 -0300

    refactor: events

--- src/bot/@types/events.ts
+++ src/bot/@types/events.ts
@@ -1,8 +1,13 @@
 import {ClientEvents} from 'discord.js';
 import {CustomClient} from './CustomClient';
 
+interface EventData {
+  name: keyof ClientEvents;
+  once?: boolean;
+}
+
 export abstract class Event<T extends keyof ClientEvents> {
-  abstract get name(): T;
+  constructor(public data: EventData) {}
   // eslint-disable-next-line max-len
   public abstract run(client: CustomClient, ...args: ClientEvents[T]): Promise<unknown> | unknown;
 }
