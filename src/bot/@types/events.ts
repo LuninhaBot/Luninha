5f309f24d8756b3cd7a53be227f80ff8206d686a
Davi <davipatricio@pm.me>
2022-12-18 00:21:05 -0300

    add example event & example command

--- src/bot/@types/events.ts
+++ src/bot/@types/events.ts
@@ -4,5 +4,5 @@ import {CustomClient} from './CustomClient';
 export abstract class Event<T extends keyof ClientEvents> {
   abstract get name(): T;
   // eslint-disable-next-line max-len
-  abstract run(client: CustomClient, ...args: ClientEvents[T]): Promise<unknown> | unknown;
+  public abstract run(client: CustomClient, ...args: ClientEvents[T]): Promise<unknown> | unknown;
 }
