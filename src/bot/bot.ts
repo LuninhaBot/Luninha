0e56f29afe6b627cc62f198ef646881aeecc4bae
Davi <davipatricio@pm.me>
2022-12-18 20:16:01 -0300

    add intents

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,6 +1,6 @@
 import {CustomClient} from '#types/CustomClient';
 import Cluster from 'discord-hybrid-sharding';
-import {Client} from 'discord.js';
+import {Client, GatewayIntentBits} from 'discord.js';
 import dotenv from 'dotenv';
 import {Utils} from './utils/Utils.js';
 
@@ -11,7 +11,13 @@ const clusterInfo = Cluster.Client.getInfo();
 const client = new Client({
   shards: clusterInfo.SHARD_LIST,
   shardCount: clusterInfo.TOTAL_SHARDS,
-  intents: 0,
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 }) as CustomClient;
 
 client.utils = new Utils(client);
